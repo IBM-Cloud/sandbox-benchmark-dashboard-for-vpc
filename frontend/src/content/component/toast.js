c9b3056f997fa5374ab26fa1ef2e27b41fa52ea5
gaurav-ibm-sa <gaurav.aggarval9@ibm.com>
2024-09-19 08:46:39 +0000

    Syncing changes from private repo: Thu Sep 19 08:46:39 UTC 2024

--- frontend/src/content/component/toast.js
+++ frontend/src/content/component/toast.js
@@ -1,49 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ToastNotification } from '@carbon/react';
-import { useTranslation } from "react-i18next";
-
-export default function Notification(props) {
-    const { t } = useTranslation();
-    const [notification, setNotification] = useState(null);
-    const [shownMessages, setShownMessages] = useState(new Set());
-    const errorMessage = t('serverError');
-    const tokenExpiredMessage = "This session has expired. Refresh the session by logging in.";
-
-    useEffect(() => {
-        if (props.showToastContainer) {
-            if ((props.subtitle.includes(errorMessage) && shownMessages.has(errorMessage)) || 
-                (props.subtitle.includes(tokenExpiredMessage) && shownMessages.has(tokenExpiredMessage))) {
-                return;
-            } else {
-                showNotification();
-                if (props.subtitle.includes(errorMessage)) {
-                    setShownMessages(prev => new Set(prev).add(errorMessage));
-                }
-                if (props.subtitle.includes(tokenExpiredMessage)) {
-                    setShownMessages(prev => new Set(prev).add(tokenExpiredMessage));
-                }
-                props.resetShowNotification();
-            }
-        }
-    }, [props.showToastContainer, props.subtitle, props.resetShowNotification, errorMessage, tokenExpiredMessage, shownMessages]);
-
-    const showNotification = () => {
-        const newNotification = (
-            <ToastNotification
-                aria-label={props.ariaLabel}
-                caption=""
-                kind={props.kind}
-                role={props.role}
-                statusIconDescription="notification"
-                subtitle={props.subtitle}
-                timeout={props.timeout}
-                title={props.title}
-                key={props.key}
-                className="notification-ui"
-            />
-        );
-        setNotification(newNotification);
-    };
-
-    return notification;
+export default function Notification({ toasts }) {
+  return (
+    <div className="generic-toast-container">
+      {toasts.map(toast => (
+        <ToastNotification
+          id={toast.id}
+          aria-label={toast.ariaLabel}
+          caption=""
+          kind={toast.kind}
+          role={toast.role}
+          statusIconDescription="notification"
+          subtitle={toast.subtitle}
+          timeout={toast.timeout}
+          title={toast.title}
+          className="notification-ui"
+        />
+      ))}
+    </div>
+  );
 }
