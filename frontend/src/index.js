aa27fe9953c2258601015e3d2d1949bc860bbc80
varun-kumar-ibm <Varun.Kumar.R@ibm.com>
2025-10-08 11:51:58 +0000

    Syncing changes from private repo: Wed Oct  8 11:51:58 UTC 2025

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 import App from './App';
 import './translation/i18n'
 import reportWebVitals from './reportWebVitals';
-import { unstable_FeatureFlags as FeatureFlags } from '@carbon/ibm-products';
+import { preview__FeatureFlags as FeatureFlags } from '@carbon/ibm-products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
